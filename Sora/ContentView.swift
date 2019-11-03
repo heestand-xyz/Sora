cb962d13fd95ecf6404a183c8f6a111c014cbe99
Hexagons <anton@hexagons.se>
2019-11-03 16:17:05 +0100

    main

--- Sora/ContentView.swift
+++ Sora/ContentView.swift
@@ -7,10 +7,45 @@
 //
 
 import SwiftUI
+import PixelKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            GeometryReader { geo in
+                ZStack() {
+                    LinearGradient(gradient: Gradient(colors: [.orange, .blue]), startPoint: .bottom, endPoint: .top)
+                        .mask(ZStack {
+                            Rectangle()
+                                .frame(height: geo.size.width / 2)
+                                .offset(y: -geo.size.width / 4)
+                            Circle()
+                        })
+                        .offset(y: geo.size.width / 4)
+                    Group {
+                        #if targetEnvironment(simulator)
+                        Rectangle()
+                            .foregroundColor(.black)
+                        #else
+                        CameraPIXUI()
+                        #endif
+                    }
+                        .mask(Circle())
+                        .offset(y: -geo.size.width / 4)
+                }
+            }
+            .aspectRatio(1.0 / 1.5, contentMode: .fit)
+            Picker(selection: .constant(0), label: EmptyView()) {
+                Text("Horizontal").tag(0)
+                Text("Vertical").tag(1)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            Spacer()
+            Circle()
+                .frame(width: 100, height: 100)
+            Spacer()
+        }
+            .padding(30)
     }
 }
 
