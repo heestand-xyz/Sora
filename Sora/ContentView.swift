26abc77d0990c284f4e7c559f7509d817fb266f4
Hexagons <anton@hexagons.se>
2019-11-03 17:17:24 +0100

    background

--- Sora/ContentView.swift
+++ Sora/ContentView.swift
@@ -15,51 +15,73 @@ import PixelKit
 struct ContentView: View {
     @EnvironmentObject var sora: Sora
     var body: some View {
-        VStack {
-            GeometryReader { geo in
-                ZStack() {
+        ZStack {
+            Group {
+                #if targetEnvironment(simulator)
+                LinearGradient(gradient: Gradient(colors: [.orange, .blue]), startPoint: self.sora.direction == .horizontal ? .leading : .bottom, endPoint: self.sora.direction == .horizontal ? .trailing : .top)
+                #else
+                RawNODEUI(node: self.sora.backgroundPix)
+                #endif
+            }
+                .opacity(0.25)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                GeometryReader { geo in
+                    ZStack() {
+                        Group {
+                            #if targetEnvironment(simulator)
+                            LinearGradient(gradient: Gradient(colors: [.orange, .blue]), startPoint: self.sora.direction == .horizontal ? .leading : .bottom, endPoint: self.sora.direction == .horizontal ? .trailing : .top)
+                            #else
+                            RawNODEUI(node: self.sora.finalPix)
+                            #endif
+                        }
+                            .mask(ZStack {
+                                Rectangle()
+                                    .frame(height: geo.size.width / 2)
+                                    .offset(y: -geo.size.width / 4)
+                                Circle()
+                            })
+                            .offset(y: geo.size.width / 4)
+                        Group {
+                            #if targetEnvironment(simulator)
+                            Rectangle()
+                                .foregroundColor(.black)
+                            #else
+                            RawNODEUI(node: self.sora.cameraPix)
+                            #endif
+                        }
+                            .mask(Circle())
+                            .offset(y: -geo.size.width / 4)
+                    }
+                }
+                .aspectRatio(1.0 / 1.5, contentMode: .fit)
+                Picker(selection: Binding<Int>(get: {
+                    self.sora.direction == .horizontal ? 0 : 1
+                }, set: { index in
+                    self.sora.direction = index == 0 ? .horizontal : .vertical
+                }), label: EmptyView()) {
+                    Text("Horizontal").tag(0)
+                    Text("Vertical").tag(1)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                Spacer()
+                ZStack {
                     Group {
                         #if targetEnvironment(simulator)
                         LinearGradient(gradient: Gradient(colors: [.orange, .blue]), startPoint: self.sora.direction == .horizontal ? .leading : .bottom, endPoint: self.sora.direction == .horizontal ? .trailing : .top)
                         #else
-                        RawNODEUI(node: self.sora.finalPix)
+                        RawNODEUI(node: self.sora.capturePix)
                         #endif
                     }
-                        .mask(ZStack {
-                            Rectangle()
-                                .frame(height: geo.size.width / 2)
-                                .offset(y: -geo.size.width / 4)
-                            Circle()
-                        })
-                        .offset(y: geo.size.width / 4)
-                    Group {
-                        #if targetEnvironment(simulator)
-                        Rectangle()
-                            .foregroundColor(.black)
-                        #else
-                        RawNODEUI(node: self.sora.cameraPix)
-                        #endif
-                    }
-                        .mask(Circle())
-                        .offset(y: -geo.size.width / 4)
+//                    Color.primary
+//                        .opacity(0.5)
                 }
+                    .mask(Circle())
+                    .frame(width: 80, height: 80)
+                Spacer()
             }
-            .aspectRatio(1.0 / 1.5, contentMode: .fit)
-            Picker(selection: Binding<Int>(get: {
-                self.sora.direction == .horizontal ? 0 : 1
-            }, set: { index in
-                self.sora.direction = index == 0 ? .horizontal : .vertical
-            }), label: EmptyView()) {
-                Text("Horizontal").tag(0)
-                Text("Vertical").tag(1)
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            Spacer()
-            Circle()
-                .frame(width: 100, height: 100)
-            Spacer()
+                .padding(30)
         }
-            .padding(30)
     }
 }
 
--- Sora/Sora.swift
+++ Sora/Sora.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 #if !targetEnvironment(simulator)
+import RenderKit
 import PixelKit
 #endif
 
@@ -16,10 +17,14 @@ class Sora: ObservableObject {
     #if !targetEnvironment(simulator)
     let cameraPix: CameraPIX
     let resolutionPix: ResolutionPIX
+    let feedbackPix: FeedbackPIX
+    let crossPix: CrossPIX
     let blurPix: BlurPIX
     let gradientPix: GradientPIX
     let lookupPix: LookupPIX
-    let finalPix: PIX
+    let finalPix: PIX & NODEOut
+    let backgroundPix: PIX
+    let capturePix: PIX
     #endif
     
     enum Direction {
@@ -33,9 +38,11 @@ class Sora: ObservableObject {
             case .horizontal:
                 gradientPix.direction = .horizontal
                 lookupPix.axis = .x
+                gradientPix.offset = 0.0
             case .vertical:
                 gradientPix.direction = .vertical
                 lookupPix.axis = .y
+                gradientPix.offset = 1.0
             }
             #endif
         }
@@ -46,17 +53,30 @@ class Sora: ObservableObject {
         #if !targetEnvironment(simulator)
         cameraPix = CameraPIX()
         cameraPix.view.placement = .aspectFill
+        cameraPix.view.checker = false
         
         resolutionPix = ResolutionPIX(at: .square(255))
         resolutionPix.input = cameraPix
         resolutionPix.placement = .aspectFill
+        
+        feedbackPix = FeedbackPIX()
+        feedbackPix.input = resolutionPix
+        
+        crossPix = CrossPIX()
+        crossPix.fraction = 0.95
+        crossPix.inputA = resolutionPix
+        crossPix.inputB = feedbackPix
+        
+        feedbackPix.feedPix = crossPix
 
         blurPix = BlurPIX()
-        blurPix.input = resolutionPix
-        blurPix.radius = 0.5
+        blurPix.input = crossPix
+        blurPix.radius = 1.0
 
         gradientPix = GradientPIX(at: .square(255))
         gradientPix.direction = .vertical
+        gradientPix.offset = 1.0
+        gradientPix.extendRamp = .mirror
 
         lookupPix = LookupPIX()
         lookupPix.axis = .y
@@ -64,6 +84,15 @@ class Sora: ObservableObject {
         lookupPix.inputB = blurPix
         
         finalPix = lookupPix
+        finalPix.view.checker = false
+        
+        backgroundPix = finalPix._nil()
+        backgroundPix.view.placement = .fill
+        backgroundPix.view.checker = false
+        
+        capturePix = finalPix._nil()
+        capturePix.view.checker = false
+        
         #endif
         
     }
