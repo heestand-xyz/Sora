d3f5321477faf035c6cdb0409a1d9c59f74af7d6
Hexagons <anton@hexagons.se>
2019-11-03 15:57:10 +0100

    Initial Commit

---
+++ Sora/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sora
+//
+//  Created by Hexagons on 2019-11-03.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
